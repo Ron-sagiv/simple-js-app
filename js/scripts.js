f35509ce64221fcc062d2320ba187714e6fac329
Ron-sagiv <pedrozeeg@gmail.com>
2025-11-14 17:08:59 +0100

    feat(js): add Pokémon list data structure
    Added an array of Pokémon objects containing name, type, height,
    and abilities. This will be used as the initial dataset for the project.

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+let pokemonList=[{name: 'Jigglypuff', type:'Balloon', height: 0.5, Abilities:['Cute charm','Friend guard']},
+{name: 'Alakazam', type:'Telekinesis', height:1.5 , Abilities:['Synchronize','Inner focus', 'Magic guard']},
+{name: 'Exeggutor', type:'Coconut', height:2 , Abilities:['Chlorophyll','Harvest']},
+{name: 'Snorlax', type:'Dozing', height:2.1 , Abilities:['Immunity', 'Thick fat', 'Gluttony']},
+{name: 'Miltank', type:'Dairy cow', height:1.2 , Abilities:['Thick fat', 'Scrappy', 'Sap-sipper']}
+];
