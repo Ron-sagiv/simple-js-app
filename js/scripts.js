32dec308292c5210d0aea8bb6252bb8004def306
Ron-sagiv <pedrozeeg@gmail.com>
2025-11-18 19:49:59 +0100

    feat(js): implement height check and output formatting
    Added a loop that prints each Pokémon's name and height.
    Introduced a height threshold that triggers the "That's a big one!" text,
    which is printed only once for the first qualifying Pokémon.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,33 @@
-let pokemonList=[{name: 'Jigglypuff', type:'Balloon', height: 0.5, Abilities:['Cute charm','Friend guard']},
-{name: 'Alakazam', type:'Telekinesis', height:1.5 , Abilities:['Synchronize','Inner focus', 'Magic guard']},
-{name: 'Exeggutor', type:'Coconut', height:2 , Abilities:['Chlorophyll','Harvest']},
-{name: 'Snorlax', type:'Dozing', height:2.1 , Abilities:['Immunity', 'Thick fat', 'Gluttony']},
-{name: 'Miltank', type:'Dairy cow', height:1.2 , Abilities:['Thick fat', 'Scrappy', 'Sap-sipper']}
+// pokemon list with characteristics of each one of them
+let pokemonList = [{ name: 'Jigglypuff', types: ['Normal', 'Fairy'], height: 0.5, abilities: ['Cute charm', 'Friend guard'] },
+{ name: 'Alakazam', types: 'Psychic', height: 1.5, abilities: ['Synchronize', 'Inner focus', 'Magic guard'] },
+{ name: 'Exeggutor', types: ['Grass', 'Psychic'], height: 2, abilities: ['Chlorophyll', 'Harvest'] },
+{ name: 'Snorlax', types: 'Normal', height: 2.1, abilities: ['Immunity', 'Thick fat', 'Gluttony'] },
+{ name: 'Miltank', types: 'Normal', height: 1.2, abilities: ['Thick fat', 'Scrappy', 'Sap-sipper'] }
 ];
+
+//a loop to present the pokemons (name and height) in the website
+
+
+
+let minHeightToTriggerText = 2;
+let bigText = 'That\'s a big one!'
+let isBigTextPrinted = false;
+
+for (i = 0; pokemonList[i];i++) {
+
+    let text = '<p>' + pokemonList[i].name + ' (Height: ' + pokemonList[i].height + ')';
+    if (pokemonList[i].height >= minHeightToTriggerText && !isBigTextPrinted) {
+        text += ' - ' + bigText 
+         isBigTextPrinted = true
+    }
+
+
+    text += '</p>';
+    document.write(text);
+    
+}
+
+
+
+
