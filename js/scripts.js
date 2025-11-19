0918b2a92f256587e5dfa16f63f5a266700ce77d
Ron-sagiv <pedrozeeg@gmail.com>
2025-11-19 09:21:50 +0100

    refactor(js) - fine tuning script
     changed strings to arrays and let to const

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,17 @@
 // pokemon list with characteristics of each one of them
 let pokemonList = [{ name: 'Jigglypuff', types: ['Normal', 'Fairy'], height: 0.5, abilities: ['Cute charm', 'Friend guard'] },
-{ name: 'Alakazam', types: 'Psychic', height: 1.5, abilities: ['Synchronize', 'Inner focus', 'Magic guard'] },
+{ name: 'Alakazam', types: ['Psychic'], height: 1.5, abilities: ['Synchronize', 'Inner focus', 'Magic guard'] },
 { name: 'Exeggutor', types: ['Grass', 'Psychic'], height: 2, abilities: ['Chlorophyll', 'Harvest'] },
-{ name: 'Snorlax', types: 'Normal', height: 2.1, abilities: ['Immunity', 'Thick fat', 'Gluttony'] },
-{ name: 'Miltank', types: 'Normal', height: 1.2, abilities: ['Thick fat', 'Scrappy', 'Sap-sipper'] }
+{ name: 'Snorlax', types: ['Normal'], height: 2.1, abilities: ['Immunity', 'Thick fat', 'Gluttony'] },
+{ name: 'Miltank', types: ['Normal'], height: 1.2, abilities: ['Thick fat', 'Scrappy', 'Sap-sipper'] }
 ];
 
 //a loop to present the pokemons (name and height) in the website
 // and it highlights 1 pokemon that is tall
 
 
-let minHeightToTriggerText = 2;
-let bigText = 'That\'s a big one!'
+const minHeightToTriggerText = 2;
+const bigText = 'That\'s a big one!'
 let isBigTextPrinted = false;
 
 for (i = 0; pokemonList[i];i++) {
