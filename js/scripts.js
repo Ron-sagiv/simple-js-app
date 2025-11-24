6a51928d3c205e03e475b4371cb703b1332f58d2
Ron-sagiv <pedrozeeg@gmail.com>
2025-11-24 19:24:08 +0100

    Refactor pokemonList into IIFE-based repository pattern
    - Wrapped pokemonList array inside an IIFE to avoid leaking global state
    - Created pokemonRepository variable assigned to the IIFE’s return object
    - Added public methods getAll() and add() defined with function keyword
    - Exposed only the allowed API by returning an object { getAll, add }
    - Updated the forEach loop to use pokemonRepository.getAll()
    - Ensured output in the HTML remains unchanged

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
+
+const pokemonRepository = (function(){
+
 // pokemon list with characteristics of each one of them
 let pokemonList = [{ name: 'Jigglypuff', types: ['Normal', 'Fairy'], height: 0.5, abilities: ['Cute charm', 'Friend guard'] },
 { name: 'Alakazam', types: ['Psychic'], height: 1.5, abilities: ['Synchronize', 'Inner focus', 'Magic guard'] },
@@ -5,6 +8,45 @@ let pokemonList = [{ name: 'Jigglypuff', types: ['Normal', 'Fairy'], height: 0.5
 { name: 'Snorlax', types: ['Normal'], height: 2.1, abilities: ['Immunity', 'Thick fat', 'Gluttony'] },
 { name: 'Miltank', types: ['Normal'], height: 1.2, abilities: ['Thick fat', 'Scrappy', 'Sap-sipper'] }
 ];
+
+// 4: Define functions separately using the function keyword
+function getAll(){
+   return pokemonList;
+}
+function add(item){
+    pokemonList.push(item);
+}
+// 3 + 4: Return ONLY an object with matching key:value names
+return{
+    getAll: getAll,
+    add:add
+};
+
+})();// 1: This is the IIFE part
+
+pokemonRepository.getAll().forEach(function(pokemon){
+  document.write(`${pokemon.name} (type: ${pokemon.types.join(', ')}, height: ${pokemon.height}, abilities: ${pokemon.abilities.join(', ')}))<br>`);
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //a loop to present the pokemons (name and height) in the website
 // and it highlights 1 pokemon that is tall
@@ -28,9 +70,10 @@ let pokemonList = [{ name: 'Jigglypuff', types: ['Normal', 'Fairy'], height: 0.5
     
 // }
 
-//Using for.Each in order to present all pokemons with all of their characteristica
-pokemonList.forEach(function(pokemon) {
-  document.write(`${pokemon.name} (type: ${pokemon.types.join(', ')}, height: ${pokemon.height}, abilities: ${pokemon.abilities.join(', ')})<br>`);
-});
+//****Part 1 of exe1.5*****
+//  Using for.Each in order to present all pokemons with all of their characteristics 
+// pokemonList.forEach(function(pokemon) {
+//   document.write(`${pokemon.name} (type: ${pokemon.types.join(', ')}, height: ${pokemon.height}, abilities: ${pokemon.abilities.join(', ')})<br>`);
+// });
 
 
