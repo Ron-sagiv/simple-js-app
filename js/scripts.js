c39f99b8d9402e7487bdef19da9cb5692d3c68ff
Ron-sagiv <pedrozeeg@gmail.com>
2025-11-25 09:38:28 +0100

    chore: remove unused files and code

--- js/scripts.js
+++ js/scripts.js
@@ -48,32 +48,5 @@ pokemonRepository.getAll().forEach(function(pokemon){
 
 
 
-//a loop to present the pokemons (name and height) in the website
-// and it highlights 1 pokemon that is tall
-
-//code from previous task that needs to be replaced:
-// const minHeightToTriggerText = 2;
-// const bigText = 'That\'s a big one!'
-// let isBigTextPrinted = false;
-
-// for (i = 0; pokemonList[i];i++) {
-
-//     let text = '<p>' + pokemonList[i].name + ' (Height: ' + pokemonList[i].height + ')';
-//     if (pokemonList[i].height >= minHeightToTriggerText && !isBigTextPrinted) {
-//         text += ' - ' + bigText 
-//          isBigTextPrinted = true
-//     }
-
-
-//     text += '</p>';
-//     document.write(text);
-    
-// }
-
-//****Part 1 of exe1.5*****
-//  Using for.Each in order to present all pokemons with all of their characteristics 
-// pokemonList.forEach(function(pokemon) {
-//   document.write(`${pokemon.name} (type: ${pokemon.types.join(', ')}, height: ${pokemon.height}, abilities: ${pokemon.abilities.join(', ')})<br>`);
-// });
 
 
